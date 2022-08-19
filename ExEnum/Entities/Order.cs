a81086d16817ca88a2dfa2384032da295118f4b7
Neemias Pereira <neemias.pereira3@gmail.com>
2022-08-19 16:57:24 -0300

    Fase 3 - Finzalização de classes

--- ExEnum/Entities/Order.cs
+++ ExEnum/Entities/Order.cs
@@ -4,6 +4,38 @@ namespace ExEnum.Entities
 {
     public class Order
     {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
 
+        public Order() 
+        {
+        }       
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem orderItem) 
+        {
+            Items.Add(orderItem);
+        }
+        public void RemoveItem(OrderItem orderitem) 
+        {
+            Items.Remove(orderitem);
+        }
+
+        public double Total() 
+        {
+            double sum = 0;
+            foreach (OrderItem obj in Items) 
+            {
+                sum =+ obj.SubTotal();
+            }
+            return sum;
+        }
     }
 }
